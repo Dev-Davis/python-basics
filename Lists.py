c74de5b0de1d007cd7fe3e0ac9b53f3125df9c4f
Sean Davis <sean.dev.davis@gmail.com>
2020-08-26 22:29:11 -0500

    lists in python example

---
+++ Lists.py
@@ -0,0 +1,12 @@
+# Lists are like arrays in Javascript
+
+# Creating a list with multiple types
+our_list = ["Michael", "Randy", "Jermaine", 1, 2, 3, True, False]
+print(our_list)
+
+# list example with a list as an index
+new_list = ["Sean", "Nae", ["Queinton", "Korey"], "Kortney"]
+print(new_list)
+
+# indexes of a list
+print(new_list[2])
