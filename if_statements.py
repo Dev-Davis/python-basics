3815966771101330d15243fd746a1eb5d0dbee98
Sean Davis <sean.dev.davis@gmail.com>
2020-08-26 22:23:12 -0500

    if statement example

---
+++ if_statements.py
@@ -0,0 +1,8 @@
+weather = "Sunny"
+
+if (weather == "Sunny"):
+    print("It feels good outside")
+elif (weather == "Cold"):
+    print ("It doesn't feel so good outside.")
+else:
+    print ("Unknown")
